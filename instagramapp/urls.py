cafc36c5f249e02460c2d91a79d2954b3e788cb6
honorine1 <uwurukundoh@gmail.com>
2019-10-21 11:41:44 +0200

    urls for profile path is added

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
 urlpatterns=[
-url('^$',views.index,name='index'),
-
+url(r'^$',views.index,name='index'),
+url(r'^posts/', views.posts, name='posts'),
+url(r'^new/post/$', views.new_post, name='new_post'),
+url(r'^new/post/postsToday/',views.posts_today,name='postsToday'),
+url(r'^profile/(\d+)',views.profile,name = 'profile'),
+# url(r'^update_profile',views.update_profile,name = 'update_profile'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
