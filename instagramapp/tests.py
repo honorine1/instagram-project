f43f18a161fd0948c8bb07eaf10037c821563738
honorine1 <uwurukundoh@gmail.com>
2019-10-24 10:03:24 +0200

    some tests are added

--- instagramapp/tests.py
+++ instagramapp/tests.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Image,Profile
+import datetime as dt
+
+
+# # Create your tests here.
+class ImageTestClass(TestCase):
+    #set up method
+
+    def test_save_method(self):
+        self.image.save_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_method(self):
+        self.image.delete_image()
+        self.objects.filter(image=image).delete()
+        self.assertTRue(len(images)>0)
+
